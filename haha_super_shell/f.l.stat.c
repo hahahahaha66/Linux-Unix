2ccb8c1280a97744842d787db5389d6f2775a07e
hahaha <a310608318@gmail.com>
2024-11-24 16:33:23 +0800

    11.24

--- haha_super_shell/f.l.stat.c
+++ haha_super_shell/f.l.stat.c
@@ -1,5 +1,7 @@
 #include<sys/stat.h>
 #include<stdio.h>
+//获取文件信息 存储在结构体stat中
+int stat(const char *restrict file, struct stat *restrict buf);
 int main(){
     struct stat buf;
     if(stat("test.txt",&buf)!=-1){
