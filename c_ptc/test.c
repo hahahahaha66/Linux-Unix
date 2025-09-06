0d8673069b4a457f191b428768414a26b1008c40
hahaha <a310608318@gmail.com>
2025-09-06 21:43:22 +0800

    c_hash

---
+++ c_ptc/test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n=0;
+    printf("请输入数据\n");
+    scanf("%d",&n);
+    if(n){
+        printf("hello ");
+        printf("world!\n");
+    }
+    printf("程序结束\n");
+    return 0;
+}
